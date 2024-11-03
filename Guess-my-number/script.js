daa4dcce7c5280c92d91d44f80316e1db996d536
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-03 18:57:08 +0200

    working solution

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+let randomNum = Math.floor(Math.random() * 100) + 1;
+let guess = document.getElementById("guess");
+let submit = document.getElementById("submit"); 
+let attempts = document.getElementById("attempts");
+let hint = document.getElementById("hint");
+
+submit.addEventListener("click", storeGuess);
+
+function storeGuess() {
+    let userGuess = parseInt(guess.value, 10); // Convert the guess to a number
+
+    if (userGuess === randomNum) {
+        console.log("You Got it!");
+    } else {
+        console.log("Wrong Guess, try again");
+    }
+}
